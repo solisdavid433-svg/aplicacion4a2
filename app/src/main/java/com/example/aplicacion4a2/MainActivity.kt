67f19dd97286e813374a027a1f63be3bbb4226bf
Jorge <gallonaran2004@gmail.com>
2025-11-04 10:58:34 -0600

    cambio de saludos

--- app/src/main/java/com/example/aplicacion4a2/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion4a2/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "holaaaa $name!",
+        text = "holaaaaaaa $name!",
         modifier = modifier
     )
 }
