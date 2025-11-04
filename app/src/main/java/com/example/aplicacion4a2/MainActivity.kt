cf2b2e689bb3b3d1fb085eaf0a455dabc4b3346a
david solis <davi_d_solis@live.com>
2025-11-04 10:29:58 -0600

    commit 2

--- app/src/main/java/com/example/aplicacion4a2/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion4a2/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Aplicacion4a2Theme {
-        Greeting("Android")
+        Greeting("Android ")
     }
 }
